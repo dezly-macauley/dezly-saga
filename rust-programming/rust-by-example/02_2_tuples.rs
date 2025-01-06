73a0e73a7d00597c9afb496ee25900f439b96d1c
dezly-macauley <dezlymacauley@proton.me>
2025-01-06 19:11:07 +0200

    End of Rust evening study session

--- rust-programming/rust-by-example/02_2_tuples.rs
+++ rust-programming/rust-by-example/02_2_tuples.rs
@@ -1,2 +1,15 @@
 // NOTE: Tuples
-// A collection of values of different types
+// 1. Can hold values of different types
+// 2. Can hold any number of values 
+
+
+// NOTE: Tuples can be used when you want 
+// a function to return multiple values
+
+fn double_both_scores(score_one: i32, score_two: i32) -> (i32, i32) {
+    let doubled_score_one = score_one * 2;
+    let doubled_score_two = score_two * 2;
+    
+    // In Rust the last line of a function is automatically returned
+    (doubled_score_one, doubled_score_two)
+}
---
+++ rust-programming/rust-by-example/12_cargo.rs
@@ -0,0 +1,8 @@
+// NOTE: Cargo
+// 1. This is the official Rust package management tool 
+
+/*
+    To search for packages:
+    https://crates.io/
+
+*/
