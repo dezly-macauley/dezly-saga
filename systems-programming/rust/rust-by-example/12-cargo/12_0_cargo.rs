e2bb0561cca02eef0502b0a4e41dca1eae11dd18
dezly-macauley <dezlymacauley@proton.me>
2025-01-17 15:07:06 +0200

    Learned how to create projects with cargo

---
+++ systems-programming/rust/rust-by-example/12-cargo/12_0_cargo.rs
@@ -0,0 +1,12 @@
+// SECTION: Cargo
+// 1. This is the official Rust package management tool 
+
+// NOTE: To seach for packages
+/*
+    https://crates.io/
+*/
+
+// NOTE: To learn more about Cargo 
+/*
+    https://doc.rust-lang.org/cargo/
+*/
--- systems-programming/rust/rust-by-example/12_cargo.rs
+++
@@ -1,8 +0,0 @@
-// NOTE: Cargo
-// 1. This is the official Rust package management tool 
-
-/*
-    To search for packages:
-    https://crates.io/
-
-*/
