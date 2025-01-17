29b9c1f7c2cdf2a1a1d29861f8033bc02a7722a6
dezly-macauley <dezlymacauley@proton.me>
2025-01-17 08:13:43 +0200

    Learned how scalar types work in Rust

---
+++ systems-programming/rust/rust-by-example/02-primitives/02_0_primitives.rs
@@ -0,0 +1,95 @@
+fn main() {
+
+    //_________________________________________________________________________
+    // SECTION: Scalar Types
+
+    //_________________________________________________________________________
+    // SUB_SECTION: Signed Integers
+    // 1. A positive or negative number with no decimals
+    // 2. The available types are:
+    // i8, i16, i32, i64, i128 and isize (pointer size)
+    // 3. Signed integers default to `i32` if you don't specify
+
+    let player_score: i32 = 500;
+    println!("player_score: {}", player_score);
+    // player_score: 500
+
+    let city_temperature: i32 = -5;
+    println!("city_temperature: {}", city_temperature);
+    // city_temperature: -5
+
+    //_________________________________________________________________________
+    // SUB_SECTION: Unsigned Integers
+    // 1. Only positive numbers with no decimals 
+    // 2. The available types are:
+    // u8, u16, u32, u64, u128 and usize (pointer size)
+    // 3. Unsigned integers default to `f64` if you don't specify
+
+    let account_balance: f64 = 821.50;
+    println!("account_balance: {}", account_balance);
+    // account_balance: 821.5
+    
+    //_________________________________________________________________________
+    // SUB_SECTION: Boolean (A variable that can be true or false)
+
+    let is_premium_user: bool = true;
+    println!("is_premium_user: {}", is_premium_user);
+    // is_premium_user: true
+    
+    //_________________________________________________________________________
+    // SUB_SECTION: The char type 
+    // Any single Unicode character
+
+    // Letters: 'A', 'z', 'é'
+    // Digits: '1', '9'
+    // Punctuation: '!', ',', '.'
+    // Symbols: '@', '#', '$'
+    // Whitespace: ' ', '\n' (newline), '\t' (tab)
+    // Emojis: '🙂', '🌟'
+    // Unicode Characters: '中', 'λ', 'Ω'
+    
+    // char Unicode scalar values like 'a', 'α' and '∞' (4 bytes each)
+
+    // NOTE: Put char values inside of single qoutes (and not double qoutes)
+
+    let emoji_choice: char = '🍑';
+    println!("emoji_choice: {}", emoji_choice);
+    // emoji_choice: 🍑
+
+    //_________________________________________________________________________
+    // SUB_SECTION: The unit type `()`
+    // The unit type is basically an explicit variable type that means 
+    // "This thing does not return anything". 
+    // In other programming languages like C or Zig, 
+    // this concept would be called `void`
+
+    // The unit type (), whose only possible value is an empty tuple: ()
+
+    // NOTE: Despite the value of a unit type being a tuple, 
+    // it is not considered a compound type 
+    // because it does not contain multiple values.
+
+    // This is not the most practical use of a unit type.
+    // The unit type is usually used by functions
+
+    // TODO: I will discuss this further in the `functions section`
+
+    let does_not_return_anything: () = ();
+    println!("does_not_return_anything: {:?}", does_not_return_anything);
+    // does_not_return_anything: ()
+
+    // NOTE: The unit type does not implement `Display` so to print it,
+    // you need to use the `debug` formater `{:?}`
+    
+    //_________________________________________________________________________
+    // SECTION: Compound Types
+
+    // Arrays like [1, 2, 3]
+    // Tuples like (1, true)
+
+
+    // SECTION: Compound Types
+    // 1. Arrays
+    // 2. Tuples
+
+}
