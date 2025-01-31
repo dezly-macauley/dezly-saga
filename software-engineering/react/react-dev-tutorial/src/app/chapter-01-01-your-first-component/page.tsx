bf7f60184d0b8328bdebdae243422d7aa5b1db8b
dezly-macauley <dezlymacauley@proton.me>
2025-01-31 10:13:22 +0200

    Learned how to create a component in React

--- software-engineering/react/react-dev-tutorial/src/app/chapter-01-01-your-first-component/page.tsx
+++ software-engineering/react/react-dev-tutorial/src/app/chapter-01-01-your-first-component/page.tsx
@@ -1,46 +1,45 @@
-// NOTE: `export default`
-// 1. This is standard JavaScript syntax (Not specific to React)
-// 2. It lets you mark the main function in a file so that you can later
-// import it into another file.
-
-// NOTE: `function Profile() {}` 
-// This is a standard JavaScript function
-
-// WARNING: React components are regular JavaScript functions but their
-// names must start with a capital letter or they won't work.
-
 //_____________________________________________________________________________
-// SECTION: A React component that returns one line of markup
+// SECTION: Step 1 - Create the component
 
-// export default function Profile() {
-//   // This is a React component that returns one thing
-//   return <img src="/katherine.png" alt="Katherine Johnson"/>;
-// }
+// NOTE: `function Profile(){return();}`
+// ---------------------------------------
+// 1. A React component is a TypeScript / JavaScript function 
+// that returns multiple lines of HTML
+// 2. The name of the React component must start with a capital letter 
+// 3. The html contents must be placed inside `return();` 
 
-//_____________________________________________________________________________
-// SECTION: A React component that returns multiple line of markup
-// 1. Use `return();`
-
+// The reason why there is no `export default` before the word 
+// function is because I only need this component to be used within this file
 function Profile() {
   return (
-    <div>
-      <img src="/katherine.png" alt="Katherine Johnson"/>
-    </div>
+    <img
+      src="/katherine.png" 
+      alt="Katherine Johnson"
+    />
   );
 }
 
 //_____________________________________________________________________________
+// SECTION: Step 2 - Create the Amazing
+
+
+// NOTE: `export default`
+// ------------------------------
+// 1. This is standard TypeScript / JavaScript syntax (Not specific to React)
+// 2. It lets you mark the main function in a file so that you can later
+// import it into another file.
 
 export default function Gallery() {
-  return(
+  return (
     <section>
-      <h1>Amazing Scientists</h1>
+      <h1>Amazing scientists</h1>
+
+      {/* NOTE: This is how to to use a React component
+      */}
+
       <Profile />
       <Profile />
       <Profile />
     </section>
   );
 }
-
-
-//_____________________________________________________________________________
